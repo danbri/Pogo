9f4e26a011fb83491a9e491ce5cc62041eacf710
Dan Brickley <danbri@danbri.org>
2010-12-08 18:00:31 +0100

    working on uri shortening

--- php/webtest.php
+++ php/webtest.php
@@ -8,27 +8,19 @@ h4 { background: #ffe; border-style: groove; border-width: thin; margin-left: .5
 .ogfield { font-weight: bold; background: #CCF;}
 </style>
 <?php 
-require_once 'plugins/viz/header.php';
-?>
-
+require_once 'plugins/viz/header.php';   ?>
 </head>
-<body>
-
-
-<h1>OpenGraph checker</h1>
-
+<body><h1>OpenGraph checker</h1>
 <p>See also Facebook's <a href="http://developers.facebook.com/tools/lint/?url=http://developers.facebook.com/tools/lint/examples/good">linter</a>.</p>
 
-
-<!-- 
-messy output ...
+<!-- driven by testcases with json metadata:
 meta: { "url": "http://developers.facebook.com/tools/lint/examples/bad_type", "testid": "bad_type", "testgroup": "fb/examples", "cache_date": "Sun 14 Nov 2010 13:46:47 CET", 
-# "valid_html": false, "uses_rdfa": true, "uses_og": true, "extended": true, "triple_count": 0, "warning": "Your og:type may only contain lowercase letters, _ and :. i.e. it must match [a-z_:]+", "home_site": "http://www.facebook.com/" }
- Expected triples: 0 Actual triples: TODO
--->
+# "valid_html": false, "uses_rdfa": true, "uses_og": true, "extended": true, "triple_count": 0, "warning": "Your og:type may only contain lowercase letters, _ and :. i.e. it must match [a-z_:]+", "home_site": "http://www.facebook.com/" } -->
 <?php 
-
 require_once 'OGDataGraph.php'; 
+$l = OGDataGraph::$nslist = loadNamespaceList();
+
+print "LOADED NAMESPACES: ". OGDataGraph::$nslist;
 
 #$suite = 'testcases/imdb_tests.xml';
 #$suite = 'testcases/basic_tests.xml'; # todo: are these loading ok?
@@ -46,11 +38,13 @@ print '<div id="canvas"></div><button id="redraw" onclick="redraw();">redraw</bu
 
 print "<p>Found ". sizeof($tests) .  " item(s).</p>";
 
-# todo: what's the url?
 
 foreach ($tests as $test) {  
   print "<h3>Test: $test</h3></h3>";
+  #print "LOADED NAMESPACES: ". OGDataGraph::$nslist;
   $og = new OGDataGraph();
+  #print "LOADED NAMESPACES: ". OGDataGraph::$nslist;
+
   $og->readTest($test); # load JSON description of this test case
   #print "<h4>rapperCheck</h4>";
   # print $og->rapperCheck();
@@ -80,11 +74,56 @@ print "Count: ". sizeof($rdf) . "<br/>\n";
 
 $viznodes = array();
 $vizedges = array();
+$vizlabels = array();
+
+function shorter($p) {
+  print "Shortening '$p' using ". sizeof($vizlabels);
+  if ($vizlabels[$p]) { print "YUP"; return ($vizlabels[$p]); } else { print "NAH"; return $p; }
+}
+
+
 print "<ul>";
+
+# loop through our RDF
 foreach ($rdf as $key => $value) {
-         print "<li>Factoid: " . $value['s'] . " " . $value['p'] . " " . $value['o'] . "</li> \n";
-#         print "short name for ".$value['p']. " ?<br/>";
-         print shortname($value['p']);
+         print "<li style='font-size: small;'>" . $value['s'] . " " . $value['p'] . " " . $value['o'] . "<br/>\n";
+         $predicate = $value['p'];
+
+#        print "<li>NEW: ".  $value['s'] . " " . $value['p'] . " " . $value['o'] . "</li>";
+
+
+
+
+	 # this loop thru predicates belongs elsewhere! todo: push into class methods
+
+         foreach (OGDataGraph::$nslist as $prefix => $uri) {
+#           print "DOES $predicate CONTAIN $uri ? <br/>";
+           if(strstr($predicate , $uri ) ) { 
+		# print '!!';
+		# php.net/manual/en/function.str-replace.php 
+		# mixed str_replace ( mixed $search , mixed $replace , mixed $subject [, int &$count ] )
+             $short = str_replace( $uri, $prefix . ':', $predicate ); # abbreviate
+		print "Replacing $uri with $prefix in $predicate : result is $short<br/>";
+             $vizlabels[$predicate] = $short;
+	     print "labels: ".sizeof($vizlabels) ."\n"; 
+             $pred_label = '';
+             if ($vizlabels[$p]) { 
+               $pred_label = $vizlabels[$p];
+             } else { 
+               $pred_label = $p;
+             }
+             print "TADA: $pred_label</br>";
+          }
+         } # end loop thru namespaces; todo: cache elsewhere!
+
+
+
+
+
+        print "<li>shorter: ".  $value['s'] . " " . shorter( $predicate) . " " . $value['o'] . "</li>";
+
+
+ 	print "</li>\n";
 }
 print "</ul>";
 ?>
@@ -97,8 +136,8 @@ var width = 600;
 window.onload = function() {
     var g = new Graph();
     st1 = {directed:true, label : "seeAlso"};
-    st2 = {directed:true, label : "Label2"};
-    g.addEdge("kiwi", "penguin",st2);
+    st2 = {directed:true, label : "location"};
+    g.addEdge("cinema", "cineplex1",st2);
     g.addEdge("kiwi", "34",st1);
     g.addEdge("34", "cherry", { directed : true, label: "fruity" } );    /* a directed connection, using an arrow */
     //    g.addEdge("id35", "apple", { stroke : "#bfa" , fill : "#56f", label : "Meat-to-Apple" });
