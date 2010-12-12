b5077eb3a035eb9e17c98a502000b99baa9b24dd
Dan Brickley <danbri@danbri.org>
2010-12-12 23:26:19 +0100

    Loading failure codes and labels from l18n file.

--- checker/OG_L18N.php
+++ checker/OG_L18N.php
@@ -3,11 +3,17 @@
 # TODO: review best practice for l18n of PHP doc strings, and setup 
 # so these can be translated and accessed.
 
-$labels = array( 'BAD_TYPE_CHARS_FAIL' => 'Poor type name found. Please avoid capitals and punctuation except ":" and "_".' ,
+Class Label 
+  { 
+    public static $messages = array( 
+        'BAD_TYPE_CHARS_FAIL' => 'Poor type name found. Please avoid capitals and punctuation except ":" and "_".' ,
 	'NONDIGIT_APPID_CHARS_FAIL' => 'fb:app_id contains non-numeric characters (perhaps api key instead?).' 
-
+# FAILED_PAGEID_NUMBERSONLY_REGEX
+# add more from checker class...
 	);
 
-function label($l) {
-  return($labels[$l]); # monolingual for now.
+  function label($l) {
+    return($labels[$l]); # monolingual for now.
+  }
+
 }
