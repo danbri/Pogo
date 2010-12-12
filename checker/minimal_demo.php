84810b107a280459234ef25da4e78343ed55d5e5
Dan Brickley <danbri@danbri.org>
2010-12-12 20:10:57 +0100

    ok

---
+++ checker/minimal_demo.php
@@ -0,0 +1,22 @@
+<html><head><title>minimal demo</title></head>
+<body>
+<?php 
+require_once 'OGDataGraph.php'; 
+
+$url = 'http://www.rottentomatoes.com/m/blade_runner/';
+$og = new OGDataGraph();
+
+try {
+#$og->readFromURL('full', $url); # real RDFa parser
+$og->readFromURL('lite', $url);  # just look for OG markup
+} catch (Exception $e) {
+  print "Parsing failed: ".$e;
+}
+
+print "<p>Title is " . $og->og_title ."</p>";
+print "<p>RDFa triple count: " . sizeof( $og->triples ) . "</p>";
+print $og->simpleTable();
+
+?>
+</body>
+</html>
