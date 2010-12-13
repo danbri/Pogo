c6d3d56ccf4fb7b4ade4de7b105b8a21e7731051
Dan Brickley <danbri@danbri.org>
2010-12-13 19:09:26 +0100

    using sitemaps.org protocol now

--- checker/testcases/mksitemap.rb
+++ checker/testcases/mksitemap.rb
@@ -3,7 +3,7 @@
 # usage mksitemap.rb > testcases/_all.xml
 
 puts '<?xml version="1.0" encoding="UTF-8"?>'
-puts '<urlset xmlns="http://www.google.com/schemas/sitemap/0.84">'
+puts '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">'
 
 
 
