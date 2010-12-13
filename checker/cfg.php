bf779b92c38f3a157b669b9aeaaeafe07e12409b
Dan Brickley <danbri@danbri.org>
2010-12-13 18:32:04 +0100

    config

---
+++ checker/cfg.php
@@ -0,0 +1,12 @@
+<?php
+
+
+# This file contains locally configured properties
+# There aren't many. 
+
+# OGDataGraph::$my_base_uri = 'http://og.danbri.org/pogo/Pogo/checker/'; 
+# (default value is 'http://localhost/pogo/Pogo/checker')
+
+# OGDataGraph::$my_base_uri = 'http://og.danbri.org/pogo/Pogo/checker/';
+
+?>
