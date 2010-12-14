b0e94e8a41fffce438cf6765ce699c852c15304e
Dan Brickley <danbri@danbri.org>
2010-12-14 16:21:03 +0100

    slightly prettier

--- checker/sitemap.php
+++ checker/sitemap.php
@@ -32,55 +32,75 @@ $me = 'index.php';
 <body>
 <?php 
 
-print "Fetching sitemap XML.<br/>";
-
 $map = 'testcases/approved.xml';
 $map = 'testcases/fb_tests.xml';
+$map = 'testcases/_all.xml';
+#$map = 'testcases/_todo.xml';
 
 require_once 'OGDataGraph.php';
-print "<hr/>";
+
 try { 
 $tests = OGDataGraph::getTests($map);
 } catch (Exception $e) { print "oops"; print $e; }
-print $tests;
 
 foreach ($tests as $tc) {
   # set up a graph for this test
   $og = new OGDataGraph();
   print "<h4>Test: $tc</h4>";
-  print "<p>Loading test.</p>";
   $og->readTest($tc);
   $url = $og->meta['url'];
 
-  print "URL now: $url";
+  if ($og->meta['warning']) { 
+    print "<em class='warning'>".$og->meta['warning']."</em><br/>\n";  
+  } 
+
+  print "Input URL: $url";
+
+
   print "<p>Expected triples: ". $og->meta['triple_count']."</p>";
   try {
      $og->readTest($tc);
      try {
        $og->readFromURL('full', $url); 
-
+       print "<p>Actual triples: ". sizeof($og->triples)."</p>\n";
        $xmlns = $og->namespaces(); 
-       print $og->xmlnsTable();
-       
-       print "<h4>Checks</h4>"; 
+       # print $og->xmlnsTable();
 
-       print "<p>Expected:</p><p><i>".$og->meta['warning']."</i></p>\n<table style='background: yellow;' border='1'>";
-#       print var_dump($og->meta['warn'] );
-       foreach ($og->meta['warn'] as $expect) {
-         print "<tr><td>$expect</td><td>". $msg[$expect] . "</td></tr>\n";
+       if (sizeof($og->triples ) > 0) { 
+         print $og->simpleTable();
+       } else {
+         print "<p>Results: no OpenGraph data found.</p>";
        }
-       print '</table>';
 
-       $report = $og->checkfields(); 
-       print "<p>Found:</p>\n\n";
+      if ( sizeof($og->meta['warn']) > 0 ) { 
+         print "<h5>Expected Issues</h5>";
+         # print var_dump($og->meta['warn'] );
+         print "<table class='expected'>";
+         foreach ($og->meta['warn'] as $expect) {
+           print "<tr><td>$expect</td><td>". $msg[$expect] . "</td></tr>\n";
+         }
+         print '</table>';
+       }
+
+       try { 
+       $report = Checker::checkall($og); 
+
+       } catch (Exception $e) {
+         print "Unexpected Exception during checking: ".$e->getMessage() ."\n</br>";
+       }
+
+       print "\n<h5>Detected Issues</h5>\n\n";
+       # print "Raw report: ".var_dump($report);
        print Checker::tableFromReport($report);
+       # should be conditional on error status here
+
 
      } catch (Exception $e) {
         print "Parsing failed... <br/>";
         print $e->getMessage() .": " . $msg[$e->getMessage()] ;
      }
      # add checks for: No og: namespace declaration. Unknown og: namespace declaration.
-
+     print "<p><br/> </p>";
   } catch(Exception $e) 
   {
     $this->fail(true, "failed loading test $tc, exception:".$e);
