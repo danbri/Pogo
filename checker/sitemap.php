29648e22b5c1e734203fa3581ffeb452532cdc9b
Dan Brickley <danbri@danbri.org>
2010-12-15 18:09:20 +0100

    tidying

--- checker/sitemap.php
+++ checker/sitemap.php
@@ -3,24 +3,22 @@ header("HTTP/1.1 200 OK");
 header("Content-type: text/html"); # half-hearted attempt to do NPH
 ?><html>
 <head><title>OpenGraph checker</title><link rel="stylesheet" href="style.css" type="text/css" />
-
 <?php 
 require_once 'page_top.php';
 require_once 'OGDataGraph.php'; 
 require_once 'OG_Checklist.php';
 require_once 'cfg.php';
-
 $msg = Label::$messages;
 $base = OGDataGraph::$my_base_uri;
-$me = 'index.php';
 print '<body>';
 #$map = 'testcases/approved.xml';
 $map = 'testcases/fb_tests.xml';
 #$map = 'testcases/_all.xml';
 #$map = 'testcases/_todo.xml';
+
 try { 
 $tests = OGDataGraph::getTests($map);
-} catch (Exception $e) { print "oops"; print $e; }
+} catch (Exception $e) { print "Failed to load configured testcases sitemap file."; }
 
 foreach ($tests as $tc) {
   $og = new OGDataGraph();	  # set up a graph for this test
@@ -28,7 +26,11 @@ foreach ($tests as $tc) {
   $og->readTest($tc);
   $url = $og->meta['url'];
 
+  # Each testcase can have a textual summary of problems in the 'warning' field:
   if ($og->meta['warning']) {    print "<em class='warning'>".$og->meta['warning']."</em><br/>\n";    } 
+
+  # Each testcase defines its main URL (relative in case of repo-only files, others are cached from public links)
+  #
   print "Input URL: $url";
 
   try {
@@ -38,13 +40,15 @@ foreach ($tests as $tc) {
        $xmlns = $og->namespaces(); 
        # print $og->xmlnsTable();
 
+       # If we got some data back, display a summary table
        if (sizeof($og->triples ) > 0) { 
          print $og->simpleTable();
        } else {
          print "<p>Results: no OpenGraph data found.</p>";
        }
-       print "<p>Data items: expected=".$og->meta['triple_count']." actual=". sizeof($og->triples)."</p>\n";
+       print "<p>Expected data items: ".$og->meta['triple_count']." actual: ". sizeof($og->triples)."</p>\n";
 
+      # Each testcase can have an array of warning codes from our OG_Checklist
       if ( sizeof($og->meta['warn']) > 0 ) { 
          print "<h5>Expected Issues</h5>";
          # print var_dump($og->meta['warn'] );
@@ -55,6 +59,8 @@ foreach ($tests as $tc) {
          print '</table>';
        }
 
+
+       # Actually run some checks
        try { 
        $report = Checker::checkall($og); 
 
@@ -72,7 +78,7 @@ foreach ($tests as $tc) {
         print "Parsing failed... <br/>";
         print $e->getMessage() .": " . $msg[$e->getMessage()] ;
      }
-     # add checks for: No og: namespace declaration. Unknown og: namespace declaration.
+
      print "<p><br/> </p>";
   } catch(Exception $e) 
   {
