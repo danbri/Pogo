1c82286b4e4162ecda9d3a6fc3055435232a5c5b
Dan Brickley <danbri@danbri.org>
2010-12-17 15:21:50 +0100

    added link to testcases/sitemap

--- checker/page_top.php
+++ checker/page_top.php
@@ -4,7 +4,9 @@
 <body>
 <div class="navlinks">
 <a href="http://ogp.me/"><img class="ogp-logo" src="doc/ogp-logo.jpg" alt="OGP logo"  width="85"/></a>
-[<a href="index.php">check url</a>] [<a href="http://developers.facebook.com/tools/lint/">Facebook's linter</a>]</div>
+[<a href="index.php">check url</a>] 
+[<a href="sitemap.php">testcases</a>] 
+[<a href="http://developers.facebook.com/tools/lint/">Facebook's linter</a>]</div>
 
 <h2><a href="<?php require_once 'OGDataGraph.php'; echo OGDataGraph::$my_base_uri; ?>">OpenGraph checker</a></h2>
 
